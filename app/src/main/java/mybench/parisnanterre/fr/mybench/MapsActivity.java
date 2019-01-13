4635e3d2b9a74de67636daca81121cd8a7c23c34
Elisée ASSOHOUN <eliseeassohoun@MacBook-Air-de-Elisee.local>
2019-01-13 15:11:10 +0100

    ajout de banc en dur

--- app/src/main/java/mybench/parisnanterre/fr/mybench/MapsActivity.java
+++ app/src/main/java/mybench/parisnanterre/fr/mybench/MapsActivity.java
@@ -14,7 +14,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +31,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        
     }
 
 
@@ -36,12 +41,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         mMap = googleMap;
 
-        LatLng centerCamera = new LatLng(48.872156,2.347464);
+        LatLng centerCamera = new LatLng(48.872156,2.347464); // position de la ville de Paris
         mMap.moveCamera(CameraUpdateFactory.newLatLng(centerCamera));
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
             mMap.setMyLocationEnabled(true);
         }
+
+        // Ajouter des bancs sous forme de marker a proximité  de la position de l'utilisateur
+
+        Marker premierBanc = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(48.86125629633995, 2.3263978958129883))
+                .title("Banc1").snippet(" mon premier banc"));
+
+        Marker deuxiemeBanc = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(48.845259549865254, 2.3134374618530273))
+                .title("Banc2").snippet(" mon deuxieme banc"));
+
+        Marker troisiemeBanc = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(48.83387658166071, 2.3323631286621094))
+                .title("Banc3").snippet(" mon troisieme banc"));
+
+
+
+
     }
 
+    
 }
