932688fc1e0a5b2141a2396ac810bc61fa8d6af7
ElHassanBaghrar <37004998@u-paris10.fr>
2019-02-10 12:13:55 +0100

    suppresion d'un attribut

--- app/src/main/java/mybench/parisnanterre/fr/mybench/MapsActivity.java
+++ app/src/main/java/mybench/parisnanterre/fr/mybench/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.List;
@@ -95,6 +96,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                          .position(new LatLng(LATITUDE,LONGITUDE))
                          .title(m.get(i).getTitle())
                          .snippet(m.get(i).getSnippet())
+
 
                 );
 
