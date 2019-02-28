df2c5fd182826580f2fd1fabda3ad6cfc7690ecb
Henley74 <henley.bidon@gmail.com>
2019-02-28 23:39:09 +0100

    màj localisation user

--- app/src/main/java/mybench/parisnanterre/fr/mybench/ClosestBenchesActivity.java
+++ app/src/main/java/mybench/parisnanterre/fr/mybench/ClosestBenchesActivity.java
@@ -1,7 +1,7 @@
 package mybench.parisnanterre.fr.mybench;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +11,13 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
+import java.util.ArrayList;
+
 public class ClosestBenchesActivity extends Activity {
     ListView listView ;
+    ArrayList<Bench> listeBancs = new ArrayList<Bench>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +27,34 @@ public class ClosestBenchesActivity extends Activity {
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.list);
 
+
+        listeBancs.add(new Bench(48.82558556529131, 2.2970748146101685, "Banc porte de Vanves"));
+        listeBancs.add(new Bench(48.84710111188743, 2.3696500305665693, "Banc avenue Ledru-Rollin"));
+        listeBancs.add(new Bench(48.83248316220944, 2.3868537740028204, "Banc à Bercy"));
+        listeBancs.add(new Bench(48.84839432099857, 2.3957903182003686, "Banc à Nation"));
+        listeBancs.add(new Bench(48.84419482018623, 2.4398082679503297, "Banc chateau de Vincennes"));
+        listeBancs.add(new Bench(48.82967862984375, 2.4008100192619177, "Banc porte de Charenton"));
+        listeBancs.add(new Bench(48.852372565095216, 2.3695847475448555, "Banc à Bastille"));
+        listeBancs.add(new Bench(48.85965097919809, 2.3721461680849063, "Banc à Richard Lenoir"));
+
+        Location currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
         // Defined Array values to show in ListView
-        String[] values = new String[] {
-                "Banc 1, disponible a 20 mètres",
-                "Banc 2, disponible a 40 mètres",
-                "Banc 3, disponible a 50 mètres",
-                "Banc 4, disponible a 60 mètres",
-                "Banc 5, disponible a 80 mètres",
-                "Banc 6, disponible a 100 mètres",
-
-        };
-
+        /*String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };*/
+        ArrayList<String> values = new ArrayList<String>();
+        int i = 0;
+        while (i < listeBancs.size()){
+            values.add(listeBancs.get(i).getNom());
+            i++;
+        }
         // Define a new Adapter
         // First parameter - Context
         // Second parameter - Layout for the row
@@ -61,10 +83,11 @@ public class ClosestBenchesActivity extends Activity {
 
                 // Show Alert
                 Toast.makeText(getApplicationContext(),
-                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        "Ce banc est à :"+listeBancs.get(position).getX()+";"+ listeBancs.get(position).getY()+" et se trouve à "+ listeBancs.get(position).getDistance() , Toast.LENGTH_LONG)
                         .show();
-                Intent i = new Intent(getApplicationContext(), RateBenchActivity.class);
-                startActivity(i);
+
+
+
             }
 
         });
